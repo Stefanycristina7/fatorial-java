2084045feba045c63f03c294490786df68bb29a0
e <stefanycristinas009@gmail.com>
2026-03-12 23:14:26 -0300

    feat: implementar cálculo de fatorial

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
+
+        while (true) {
+            long fatorial = 1;
+            System.out.print("Digite um número inteiro positivo para calcular o fatorial:");
+            int numero = entrada.nextInt();
+            if(numero < 0){
+                System.out.println("ERRO! Fatorial é um número natural inteiro positivo!!");
+                continue;
+            }
+            System.out.print(numero+"!= ");
+            for (int i = numero; i >= 1; i--) {
+                fatorial *= i;
+                System.out.print(i);
+                if (i > 1) {
+                    System.out.print(" x ");
+                }
+            }
+            System.out.println("= "+fatorial);
+
+
+        }
     }
 }
