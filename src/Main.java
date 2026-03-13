48891e31719d6847cda0e0d6ceef2b295c88f0a4
e <stefanycristinas009@gmail.com>
2026-03-12 23:23:01 -0300

    feat: adicionar opção de calcular o fatorial de outro número

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        String continuacao;
 
 
         while (true) {
             long fatorial = 1;
             System.out.print("Digite um número inteiro positivo para calcular o fatorial:");
             int numero = entrada.nextInt();
+            entrada.nextLine(); //evitar bug do Scanner
+
+            //validar entrada de valor positivo
             if(numero < 0){
                 System.out.println("ERRO! Fatorial é um número natural inteiro positivo!!");
                 continue;
             }
             System.out.print(numero+"!= ");
+            //estrutura de repetição para calcular o fatorial e exibir cálculo
             for (int i = numero; i >= 1; i--) {
                 fatorial *= i;
                 System.out.print(i);
@@ -22,6 +27,13 @@ public class Main {
                 }
             }
             System.out.println("= "+fatorial);
+
+            System.out.println("Deseja calcular outro fatorial? [s/n]");
+            continuacao = entrada.nextLine();
+            if(!continuacao.equalsIgnoreCase("s")){
+                System.out.println("Até a próxima! ");
+                break;
+            }
 
 
         }
